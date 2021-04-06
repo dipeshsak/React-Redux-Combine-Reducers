d680176ea9b4d989fba95e7dda49b881ca610709
Dipesh Sakharkar <dipeshsakharkar1@gmail.com>
2021-04-06 15:44:50 +0530

    Basic code to update in a and b

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {connect} from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+
+  state={
+    a:0,
+    b:0
+  }
+  
+  render(){
+    return(
+      <div>
+        <h1 style={{textAlign:'center'}}>Welcome</h1>
+      
+      <div className="App" style={{display:'flex',flexDirection:'row',alignItems:'center',justifyContent:'space-around'}}>
+         
+         <div>
+           <h2>A : {this.props.a}</h2>
+           <button onClick={this.props.updateA}>Update A</button>
+         </div>
+         <div>
+           <h2>B : {this.props.b}</h2>
+           <button onClick={this.props.updateB}>Update B</button>
+         </div>
+      </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps =(state)=>{
+  return{
+    a:state.a,
+    b:state.b
+  }
+}
+
+const mapDispatchToProps =(dispatch)=>{
+  return{
+    updateA:()=>dispatch({type:'UPDATE_A'}),
+    updateB:()=>dispatch({type:'UPDATE_B'})
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App)
